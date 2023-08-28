876d85aa4b6eddfc55556000b7e04fba42664c46
Mitankshi <114574805+Mitankshi@users.noreply.github.com>
2023-08-28 20:21:09 +0530

    Add files via upload

---
+++ task ques 11.sql
@@ -0,0 +1,33 @@
+use mita
+CREATE TABLE Student (
+    ID INT PRIMARY KEY NOT NULL,
+    Name VARCHAR(20) NOT NULL,
+    Age INT NOT NULL,
+    Address VARCHAR(25)
+);
+-- Insert 5 records into the Student table
+INSERT INTO Student (ID, Name, Age, Address)
+VALUES
+    (1, 'John Doe', 20, '123 Main St'),
+    (2, 'Jane Smith', 22, '456 Elm St'),
+    (3, 'Bob Johnson', 19, '789 Oak St'),
+    (4, 'Alice Brown', 21, '101 Maple St'),
+    (5, 'Eve White', 23, '202 Pine St');
+
+select * from student;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
